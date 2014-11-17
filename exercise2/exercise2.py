6c44355803b2fbb65f10ca08de807ff722c517df
Luka Stout <lukastout@gmail.com>
2014-11-17 14:10:07 +0100

    Added comments and change starting position and contour value

--- exercise2/exercise2.py
+++ exercise2/exercise2.py
@@ -1,75 +1,78 @@
 #!/usr/bin/env python
-
 import vtk
-import sys
-import time
-import os
 
 
+#Starting value for the contourfilter
 initial_v = 2750
+#Min value slider
 min_v = 500
+#Max value slider
 max_v = 5000
 
+
+
+#Creation of renderer, window and iteractor
 ren = vtk.vtkRenderer()
 renWin = vtk.vtkRenderWindow()
 renWin.AddRenderer(ren)
 iren = vtk.vtkRenderWindowInteractor()
 iren.SetRenderWindow(renWin)
 
+
+
+#====Reading in the data using a Volume16Reader
 v16 = vtk.vtkVolume16Reader()
 v16.SetDataDimensions(256,256)
-v16.SetFilePrefix(os.cwd()+"MysteryData/slice")
+v16.SetFilePrefix("MysteryData/slice")
 v16.SetImageRange(1, 94)
 v16.SetDataSpacing(1, 1, 2)
+#==============================================
 
-
-#Creation of Jaw
+#====Creation of the contour filter ============
+# This is the variable contour filter
 contour_skeleton = vtk.vtkContourFilter()
 contour_skeleton.SetInputConnection(v16.GetOutputPort())
 contour_skeleton.SetValue(0, initial_v)
 contour_skeleton.ComputeNormalsOn()
-# Turn off blue color from contour -> Set color in Actor
-# contour_skeleton.ComputeScalarsOff()
 
+# Create normals for smoother rendereing
 normals_skeleton = vtk.vtkPolyDataNormals()
 normals_skeleton.SetInputConnection(contour_skeleton.GetOutputPort())
 normals_skeleton.SetFeatureAngle(60.0)
 
-
+# Map the data to a certain range
 mapper_skeleton = vtk.vtkPolyDataMapper()
 mapper_skeleton.SetScalarRange(500,5000)
 mapper_skeleton.SetInputConnection(normals_skeleton.GetOutputPort())
 
+# Create the actor used for coloring
 skeleton = vtk.vtkActor()
 skeleton.SetMapper(mapper_skeleton)
 
-# Turn this on if the computation of scalars is off
-# skeleton.GetProperty().SetColor(.7,.7,.7)
-# skeleton.GetProperty().SetAmbient(.1)
-# skeleton.GetProperty().SetDiffuse(1)
-# skeleton.GetProperty().SetSpecular(.1)
+ren.AddActor(skeleton)
+#==============================================
 
-
-
-#Creation of Skin
+#====Creation of the opaque skin ==============
 contour_skin = vtk.vtkContourFilter()
 contour_skin.SetInputConnection(v16.GetOutputPort())
-contour_skin.SetValue(0, 500)
+contour_skin.SetValue(0, 500) #500 is the value for skin
 contour_skin.ComputeNormalsOn()
-# Turn off blue color from contour -> Set color in Actor
-contour_skin.ComputeScalarsOff()
-
+contour_skin.ComputeScalarsOff() #remove blue color
 
 mapper_skin = vtk.vtkPolyDataMapper()
-# Turn off blue color
-# mapper.ScalarVisibilityOff()
 mapper_skin.SetInputConnection(contour_skin.GetOutputPort())
 
 skin = vtk.vtkActor()
 skin.SetMapper(mapper_skin)
-skin.GetProperty().SetColor(.5,.5,.5)
+
+# Set skin color and opacity. Is transparent to show to data inside in context
+skin.GetProperty().SetColor(.7,.7,.7)
 skin.GetProperty().SetOpacity(.2)
 
+ren.AddActor(skin)
+#==============================================
+
+#====Creation of a bounding box================
 outlineData = vtk.vtkOutlineFilter()
 outlineData.SetInputConnection(v16.GetOutputPort())
 mapOutline = vtk.vtkPolyDataMapper()
@@ -78,35 +81,39 @@ outline = vtk.vtkActor()
 outline.SetMapper(mapOutline)
 outline.GetProperty().SetColor(0.5, 0.5, 0.5)
 
+ren.AddActor(outline)
+#==============================================
+
+#====Camera====================================
 aCamera = vtk.vtkCamera()
 aCamera.SetViewUp(0, 0, -1)
-aCamera.SetPosition(0, 1, 0)
+aCamera.SetPosition(-1, 1, 0)
 aCamera.SetFocalPoint(0, 0, 0)
 aCamera.ComputeViewPlaneNormal()
 
-ren.AddActor(outline)
-ren.AddActor(skeleton)
-ren.AddActor(skin)
+
 ren.SetActiveCamera(aCamera)
 ren.ResetCamera()
+
 aCamera.Dolly(1.5)
+#==============================================
 
-# Set a background color for the renderer and set the size of the
-# render window (expressed in pixels).
+
+#====Change window settings====================
+# Set a background color for the renderer
 ren.SetBackground(.1, .1, .1)
+# Set windows size in pixels
 renWin.SetSize(640, 480)
-# Note that when camera movement occurs (as it does in the Dolly()
-# method), the clipping planes often need adjusting. Clipping planes
-# consist of two planes: near and far along the view direction. The
-# near plane clips out objects in front of the plane the far plane
-# clips out objects behind the plane. This way only what is drawn
-# between the planes is actually rendered.
 ren.ResetCameraClippingRange()
+#==============================================
 
-# Interact with the data.
+
+
+# Initialize the interactor and render for the first time
 iren.Initialize()
 renWin.Render()
 
+#=======Slider Creation========================
 def vtkSliderCallback2(obj, event):
     sliderRepres = obj.GetRepresentation()
     pos = sliderRepres.GetValue()
@@ -119,9 +126,9 @@ SliderRepres.SetMaximumValue(max_v)
 
 
 SliderRepres.GetPoint1Coordinate().SetCoordinateSystemToNormalizedDisplay()
-SliderRepres.GetPoint1Coordinate().SetValue(0.1, 0.1)
+SliderRepres.GetPoint1Coordinate().SetValue(0.2, 0.1)
 SliderRepres.GetPoint2Coordinate().SetCoordinateSystemToNormalizedDisplay()
-SliderRepres.GetPoint2Coordinate().SetValue(0.9, 0.1)
+SliderRepres.GetPoint2Coordinate().SetValue(0.8, 0.1)
 
 SliderRepres.SetSliderLength(0.02)
 SliderRepres.SetSliderWidth(0.03)
@@ -140,8 +147,8 @@ SliderWidget.SetAnimationModeToAnimate()
 SliderWidget.SetEnabled(True)
 SliderWidget.AddObserver("EndInteractionEvent", vtkSliderCallback2)
 
-
-
 SliderRepres.SetValue(initial_v)
-renWin.Render()
+#==============================================
+
+#Start the interaction
 iren.Start()
