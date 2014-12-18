aeea8e82434ac50731ab41034432bff1c2d3e82d
Luka Stout <lukastout@gmail.com>
2014-12-18 14:01:46 +0100

    Add Legend, Gave up on buttons

--- exercise4/exercise4.py
+++ exercise4/exercise4.py
@@ -9,19 +9,6 @@ import vtk
 
 frog_dir = sys.argv[1]
 
-
-def create_image(x_size,y_size,color):
-	image = vtk.vtkImageData()
-	image.SetDimensions(x_size,y_size,1)
-	image.AllocateScalars(vtk.VTK_FLOAT,3)
-
-	for y in xrange(x_size):
-		for x in xrange(y_size):
-			image.SetScalarComponentFromDouble(x,y,0,0,color[0])
-			image.SetScalarComponentFromDouble(x,y,0,1,color[0])
-			image.SetScalarComponentFromDouble(x,y,0,2,color[0])
-	return image
-
 ren = vtk.vtkRenderer()
 renWin = vtk.vtkRenderWindow()
 renWin.AddRenderer(ren)
@@ -36,7 +23,6 @@ frog_reader.SetFileDimensionality(2)
 frog_reader.SetDataSpacing(1,1,1.5)
 frog_reader.SetDataExtent(0,499,0,469,1,136)
 frog_reader.Update()
-
 
 frog_contour = vtk.vtkContourFilter()
 frog_contour.SetInputConnection(frog_reader.GetOutputPort())
@@ -87,6 +73,7 @@ opacity_transfer_function.AddPoint(12, .1)
 opacity_transfer_function.AddPoint(13, .1)
 opacity_transfer_function.AddPoint(14, .1)
 opacity_transfer_function.AddPoint(15, .1)
+opacity_transfer_function.AddPoint(16, 0)
 
 color_transfer_function = vtk.vtkColorTransferFunction()
 color_transfer_function.AddRGBPoint(1 , 216/255,101/255, 79/255)
@@ -117,41 +104,26 @@ volume.SetProperty(volume_prop)
 
 ren.AddVolume(volume)
 
+legend = vtk.vtkLegendBoxActor();
+legend.SetNumberOfEntries(15);
+legendBox = vtk.vtkCubeSource();
+legendBox.Update();
 
-# button_widget = vtk.vtkButtonWidget()
-# button_widget.SetInteractor(iren)
+names = ["Blood","Brain","Duodenum","Eye retina","Eye white","Heart","Ileum","Kidney","Large intestine","Liver","Lung","Nerve","Skeleton","Spleen","Stomach"]
+for i in range(1,16):
+	legend.SetEntry(i-1,legendBox.GetOutput(),names[i-1],color_transfer_function.GetColor(i))
 
-# rep = vtk.vtkTexturedButtonRepresentation2D()
-# rep.SetNumberOfStates(2)
-# rep.SetButtonTexture(0,create_image(2,2,[0.1,0.1,0.1]))
-# rep.SetButtonTexture(1,create_image(2,2,[0.1,0.1,0.1]))
-# rep.PlaceWidget([0,50,0,50,0,0])
+legend.GetPositionCoordinate().SetCoordinateSystemToView();
+legend.GetPositionCoordinate().SetValue(.6, 0.0);
 
-# class buttonCallback():
-# 	value = None
+legend.GetPosition2Coordinate().SetCoordinateSystemToView();
+legend.GetPosition2Coordinate().SetValue(1.1, 1);
 
-# 	def __init__(*args):
-# 		print args
+legend.UseBackgroundOn()
+legend.SetBackgroundColor([0,0,0])
 
+ren.AddActor(legend)
 
-# 	def Execute(self, obj, event):
-# 		print rep.GetState()
-# 		print self.value
-# 		# if self.value:
-# 		# 	current_state = opacity_transfer_function.GetValue(self.value)
-# 		# 	if current_state > 0:
-# 		# 		opacity_transfer_function.SetValue(self.value,0)
-# 		#		rep.SetButtonTexture(0,create_image(2,2,[0.1,0.1,0.1]))
-# 		# 	else:
-# 		# 		opacity_transfer_function.SetValue(self.value,.1)
-# 		#		rep.SetButtonTexture(0,create_image(2,2,[0.8,0.8,0.8]))
-
-
-# button_widget.SetRepresentation(rep)
-# cb = buttonCallback()
-# cb.value = 5
-
-# button_widget.AddObserver("Select",cb.Execute)
 
 
 aCamera = vtk.vtkCamera()
@@ -172,6 +144,5 @@ renWin.SetSize(640,480)
 #	Start visualization
 
 iren.Initialize()
-#button_widget.On()
 renWin.Render()
 iren.Start()
