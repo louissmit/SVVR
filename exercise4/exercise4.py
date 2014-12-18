0ba943314c210ef9885d392d9c01456d986a36b4
Luka Stout <lukastout@gmail.com>
2014-12-18 18:21:37 +0100

    New Colors

--- exercise4/exercise4.py
+++ exercise4/exercise4.py
@@ -7,6 +7,38 @@ if len(sys.argv) != 2:
 
 import vtk
 
+def get_reader(file_prefix):
+	reader = vtk.vtkImageReader()
+	reader.SetFilePrefix(file_prefix)
+	reader.SetFilePattern('%s%03d.raw')
+	reader.SetDataScalarTypeToUnsignedChar()
+	reader.SetFileDimensionality(2)
+	reader.SetDataSpacing(1,1,1.5)
+	reader.SetDataExtent(0,499,0,469,1,136)
+	reader.Update()
+	return reader
+
+skin_color = (0,1,0) 
+
+tissue_config = {}  
+tissue_config['Blood'] 			= (1 ,(232 /255, 0 /255, 0 /255),.7)
+tissue_config['Brain'] 			= (2 ,(180 /255, 56 /255, 180 /255),.7)
+tissue_config['Duodenum'] 		= (3 ,(255 /255, 255 /255, 68 /255),.7)	
+tissue_config['Eye retina'] 	= (4 ,(100 /255, 149 /255, 237 /255),.7)
+tissue_config['Eye white'] 		= (5 ,(232 /255, 232 /255, 232 /255),.7)		
+tissue_config['Heart'] 			= (6 ,(249 /255, 69 /255, 0 /255),.7)	
+tissue_config['Ileum'] 			= (7 ,(255 /255, 182 /255, 193 /255),.7)
+tissue_config['Kidney'] 		= (8 ,(139 /255, 69 /255, 19 /255),.7)	
+tissue_config['Large Intestine']= (9 ,(255 /255, 20 /255, 147 /255),.7) 	
+tissue_config['Liver'] 			= (10,(249 /255, 201 /255, 144 /255),.1)
+tissue_config['Lung'] 			= (11,(0 /255, 181 /255, 117 /255),.7)	
+tissue_config['Nerve'] 			= (12,(255 /255, 215 /255, 0 /255),.7)	
+tissue_config['Skeleton'] 		= (13,(255 /255, 255 /255, 255 /255), .1)	
+tissue_config['Spleen']			= (14,(188 /255, 139 /255, 172 /255),.7)
+tissue_config['Stomach'] 		= (15,(106 /255, 90 /255, 205 /255),.7)
+
+106-90-205
+
 frog_dir = sys.argv[1]
 
 ren = vtk.vtkRenderer()
@@ -15,14 +47,7 @@ renWin.AddRenderer(ren)
 iren = vtk.vtkRenderWindowInteractor()
 iren.SetRenderWindow(renWin)
 
-frog_reader = vtk.vtkImageReader() 
-frog_reader.SetFilePrefix(frog_dir + 'frog.')
-frog_reader.SetFilePattern('%s%03d.raw')
-frog_reader.SetDataScalarTypeToUnsignedChar()
-frog_reader.SetFileDimensionality(2)
-frog_reader.SetDataSpacing(1,1,1.5)
-frog_reader.SetDataExtent(0,499,0,469,1,136)
-frog_reader.Update()
+frog_reader = get_reader(frog_dir+'frog.')
 
 frog_contour = vtk.vtkContourFilter()
 frog_contour.SetInputConnection(frog_reader.GetOutputPort())
@@ -35,20 +60,11 @@ frog_mapper.SetInputConnection(frog_contour.GetOutputPort())
 frog_actor = vtk.vtkActor()
 frog_actor.SetMapper(frog_mapper)
 frog_actor.GetProperty().SetOpacity(0.1)
-frog_actor.GetProperty().SetColor(0,.5,0)
+frog_actor.GetProperty().SetColor(*skin_color)
 
 ren.AddActor(frog_actor)
 
-
-
-tissue_reader = vtk.vtkImageReader() 
-tissue_reader.SetFilePrefix(frog_dir + 'frogTissue.')
-tissue_reader.SetFilePattern('%s%03d.raw')
-tissue_reader.SetDataScalarTypeToUnsignedChar()
-tissue_reader.SetFileDimensionality(2)
-tissue_reader.SetDataSpacing(1,1,1.5)
-tissue_reader.SetDataExtent(0,499,0,469,1,136)
-tissue_reader.Update()
+tissue_reader = get_reader(frog_dir + 'frogTissue.')
 
 composite_function = vtk.vtkVolumeRayCastCompositeFunction()
 composite_function.SetCompositeMethodToClassifyFirst()
@@ -58,73 +74,47 @@ volume_mapper.SetInputConnection(tissue_reader.GetOutputPort())
 
 opacity_transfer_function = vtk.vtkPiecewiseFunction()
 opacity_transfer_function.AddPoint(0, 0)
-opacity_transfer_function.AddPoint(1, .1)
-opacity_transfer_function.AddPoint(2, .1)
-opacity_transfer_function.AddPoint(3, .1)
-opacity_transfer_function.AddPoint(4, .1)
-opacity_transfer_function.AddPoint(5, .1)
-opacity_transfer_function.AddPoint(6, .1)
-opacity_transfer_function.AddPoint(7, .1)
-opacity_transfer_function.AddPoint(8, .1)
-opacity_transfer_function.AddPoint(9, .1)
-opacity_transfer_function.AddPoint(10, .1)
-opacity_transfer_function.AddPoint(11, .1)
-opacity_transfer_function.AddPoint(12, .1)
-opacity_transfer_function.AddPoint(13, .1)
-opacity_transfer_function.AddPoint(14, .1)
-opacity_transfer_function.AddPoint(15, .1)
 opacity_transfer_function.AddPoint(16, 0)
 
 color_transfer_function = vtk.vtkColorTransferFunction()
-color_transfer_function.AddRGBPoint(1 , 216/255,101/255, 79/255)
-color_transfer_function.AddRGBPoint(2 , 250/255,250/255,225/255)
-color_transfer_function.AddRGBPoint(3 , 255/255,253/255,229/255)
-color_transfer_function.AddRGBPoint(4 , 111/255,184/255,210/255)
-color_transfer_function.AddRGBPoint(5 , 194/255,142/255,  0/255)
-color_transfer_function.AddRGBPoint(6 , 206/255,110/255, 84/255)
-color_transfer_function.AddRGBPoint(7 , 183/255,156/255,220/255)
-color_transfer_function.AddRGBPoint(8 , 185/255,102/255, 83/255)
-color_transfer_function.AddRGBPoint(9 , 204/255,168/255,143/255)
-color_transfer_function.AddRGBPoint(10, 221/255,130/255,101/255)
-color_transfer_function.AddRGBPoint(11, 197/255,165/255,145/255)
-color_transfer_function.AddRGBPoint(12, 255/255,234/255, 92/255)
-color_transfer_function.AddRGBPoint(13, 241/255,214/255,145/255)
-color_transfer_function.AddRGBPoint(14, 157/255,108/255,162/255)
-color_transfer_function.AddRGBPoint(15, 216/255,132/255,105/255)
+color_transfer_function.AddRGBPoint(0,0,0,0)
+color_transfer_function.AddRGBPoint(16,0,0,0)
 
-volume_prop= vtk.vtkVolumeProperty()
+legend = vtk.vtkLegendBoxActor()
+legend.SetNumberOfEntries(15)
+
+legendBox = vtk.vtkCubeSource()
+legendBox.Update()
+
+legend.GetPositionCoordinate().SetCoordinateSystemToView()
+legend.GetPositionCoordinate().SetValue(.55, 0.0)
+legend.GetPosition2Coordinate().SetCoordinateSystemToView()
+legend.GetPosition2Coordinate().SetValue(1, .99)
+legend.SetPadding(10)
+legend.UseBackgroundOn()
+legend.SetBackgroundColor([0,0,0])
+legend.SetBackgroundOpacity(0.5)
+
+for tissue_name, (point, tissue_color, tissue_opacity) in tissue_config.items():
+
+	opacity_transfer_function.AddPoint(point,tissue_opacity)
+	color_transfer_function.AddRGBPoint(point,*tissue_color)
+	
+	legend.SetEntry(point-1,legendBox.GetOutput(),tissue_name,tissue_color)
+
+
+
+volume_prop = vtk.vtkVolumeProperty()
 volume_prop.SetColor(color_transfer_function)
 volume_prop.SetScalarOpacity(opacity_transfer_function)
 volume_prop.SetInterpolationTypeToLinear()
-
 
 volume = vtk.vtkVolume()
 volume.SetMapper(volume_mapper)
 volume.SetProperty(volume_prop)
 
 ren.AddVolume(volume)
-
-legend = vtk.vtkLegendBoxActor();
-legend.SetNumberOfEntries(15);
-legendBox = vtk.vtkCubeSource();
-legendBox.Update();
-
-names = ["Blood","Brain","Duodenum","Eye retina","Eye white","Heart","Ileum","Kidney","Large intestine","Liver","Lung","Nerve","Skeleton","Spleen","Stomach"]
-for i in range(1,16):
-	legend.SetEntry(i-1,legendBox.GetOutput(),names[i-1],color_transfer_function.GetColor(i))
-
-legend.GetPositionCoordinate().SetCoordinateSystemToView();
-legend.GetPositionCoordinate().SetValue(.6, 0.0);
-
-legend.GetPosition2Coordinate().SetCoordinateSystemToView();
-legend.GetPosition2Coordinate().SetValue(1.1, 1);
-
-legend.UseBackgroundOn()
-legend.SetBackgroundColor([0,0,0])
-
 ren.AddActor(legend)
-
-
 
 aCamera = vtk.vtkCamera()
 aCamera.SetViewUp(100, -1, 0)
@@ -139,9 +129,6 @@ ren.GradientBackgroundOn()
 ren.SetBackground(.32,.58,.92)
 ren.SetBackground2(.6,.8,.92)
 renWin.SetSize(640,480)
-
-
-#	Start visualization
 
 iren.Initialize()
 renWin.Render()
